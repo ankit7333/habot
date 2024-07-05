374fab024ef4f4eb96a728edfe542b715fa2f03a
ankitadulkar <ankit.adulkar1@justdial.com>
2024-07-05 13:32:18 +0530

    fifth commit

--- src/features/Menu/MenuItem.jsx
+++ src/features/Menu/MenuItem.jsx
@@ -1,24 +1,19 @@
 import LinkButton from "../../ui/LinkButton"
 
 function MenuItem({menu, handleMenu}) {
-    console.log(menu, "menumenu")
-    return (    
-            <li onClick={handleMenu} className="relative text-[15px] capitalize text-stone-500">
-                {Object.keys(menu).map((key,id) => (
-                    <LinkButton key={id} type="transparent">{key !== 'additional' && menu[key]}</LinkButton>
-                ))}
-            {/* {
-                menu.additional.length > 0 &&(
-                    <ul className="absolute z-10">
-                        {
-                            menu.additional.map((additionalItem, subIndex) => (
-                                <li key={subIndex}><LinkButton type="transparent">{additionalItem}</LinkButton></li>
-                            ))
-                        }
-                    </ul>
-                )
-            } */}
-        </li>
+    return ( 
+        <li onClick={handleMenu} className={`group relative text-[15px] capitalize text-stone-500 cursor-pointer`}>
+            {menu.label}
+            {menu.additional.length ? 
+                <ul className={`absolute top-5 z-10 hidden bg-white py-2 px-2 rounded-[2px] group-hover:block`}>
+                    {menu.additional.map((val)=>{
+                        return(<li className='cursor-pointer'>
+                                <LinkButton type="transparent">{val}</LinkButton>
+                        </li>)
+                    })} 
+                </ul>
+            :null} 
+        </li>   
     )
 }
 
--- src/features/Menu/index.jsx
+++ src/features/Menu/index.jsx
@@ -14,32 +14,12 @@ const menus = [
 function Menu({isOpen, handleMenu}) {
     return (
         <ul className={`flex items-center gap-4 py-5 md:py-0 md:gap-8 flex-col md:flex-row absolute top-[60px] left-0 md:static w-full md:size-fit bg-white ${!isOpen ? 'hidden md:flex' : ''}`}>
-        {   
-        menus.map((value)=>{
-            return (
-                <li className={`group relative text-[15px] capitalize text-stone-500 cursor-pointer`}>
-                    {value.label}
-                    {value.additional.length ? 
-                    <ul className={`absolute top-5 z-10 hidden bg-white py-2 px-2 rounded-[2px] group-hover:block`}>
-                       {value.additional.map((val)=>{
-                           return (
-                               <li className='cursor-pointer'>{val}</li> 
-                            )
-                        })} 
-                    </ul>
-                    :null}
-                    
-                    </li>
-            )
-        })
-            
-            // menus.map((menu, index) => (
-            //     <MenuItem key={index} menu={menu} handleMenu={handleMenu} />
-            // ))
-        }
-            <li>
-                <Button onClick={handleMenu} type="secondary">login / sign up</Button>
-            </li>
+        {menus.map((menu, index) => (
+                <MenuItem key={index} menu={menu} handleMenu={handleMenu} />
+        ))}
+        <li>
+            <Button onClick={handleMenu} type="secondary">login / sign up</Button>
+        </li>
         </ul>
     )
 }
