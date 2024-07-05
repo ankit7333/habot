fb3bcfa883a421b656fdab8f8ca603006f3455b1
ankitadulkar <ankit.adulkar1@justdial.com>
2024-07-05 10:50:20 +0530

    third commit

--- src/features/Postyourrequirement/index.jsx
+++ src/features/Postyourrequirement/index.jsx
@@ -4,7 +4,7 @@ function Postyourrequirement() {
         <section className="py-10 px-5 md:px-0 bg-[#072F57]">
             <div className="container mx-auto flex flex-col md:flex-row justify-between gap-10">
                 <div className='w-full md:w-1/2 h-80'>
-                    <iframe className="w-full h-full" src="https://www.bigbuckbunny.org/"></iframe>
+                    <iframe title="This is a unique title" className="w-full h-full" src="https://www.bigbuckbunny.org/"></iframe>
                 </div>
                 <Tab />
             </div> 
